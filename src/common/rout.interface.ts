b700a071d9b9b8fbfb40b82bd5c1c2c1fec00f22
124c4a11 <124c4a11@mail.ru>
2022-01-03 16:46:19 +0300

    Middleware for routes

--- src/common/rout.interface.ts
+++ src/common/rout.interface.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Response, Request, Router } from 'express';
 
+import { IMiddleware } from './middleware.interface';
+
 export interface IControllerRote {
   path: string;
   method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
   func: (req: Request, res: Response, next: NextFunction) => void;
+  middlewares?: IMiddleware[];
 }
 
 export type ExpressReturnType = Response<any, Record<string, any>>;
