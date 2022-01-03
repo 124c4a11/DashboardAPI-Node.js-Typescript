124fe45719219ea5886cfabafe68ff939b7bc1e0
124c4a11 <124c4a11@mail.ru>
2022-01-03 17:29:30 +0300

    User validation

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+  @IsString({ message: 'Name not specified' })
   name: string;
+
+  @IsEmail({}, { message: 'Invalid email' })
   email: string;
+
+  @IsString({ message: 'Password not specified' })
   password: string;
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -9,8 +9,8 @@ import { LoggerService } from '../logger/logger.service';
 import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { User } from './user.entity';
 import { UserService } from './users.service';
+import { ValidateMiddleware } from '../common/validate.middleware';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -21,7 +21,12 @@ export class UserController extends BaseController implements IUserController {
     super(loggerService);
 
     this.bindRouts([
-      { path: '/register', method: 'post', func: this.register },
+      {
+        path: '/register',
+        method: 'post',
+        func: this.register,
+        middlewares: [new ValidateMiddleware(UserRegisterDto)],
+      },
       { path: '/login', method: 'post', func: this.login },
     ]);
   }
