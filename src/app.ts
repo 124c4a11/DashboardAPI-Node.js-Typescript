a68c00d3b6de5850e4531d03efb213611717c1b2
124c4a11 <124c4a11@mail.ru>
2022-01-04 14:25:12 +0300

    Prisma: connection to the database

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import { IConfigService } from './config/config.service.interface';
 import { ILogger } from './logger/logger.service.interface';
 import { IExceptionFilter } from './errors/exception.filter.interface';
 import { UserController } from './users/user.controller';
+import { PrismaService } from './database/prisma.service';
 
 @injectable()
 export class App {
@@ -21,6 +22,7 @@ export class App {
     @inject(TYPES.UserController) private userController: UserController,
     @inject(TYPES.ExceptionFilter) private exceptionFilter: IExceptionFilter,
     @inject(TYPES.ConfigService) private configService: IConfigService,
+    @inject(TYPES.PrismaService) private prismaService: PrismaService,
   ) {
     this.app = express();
     this.port = 3000;
@@ -42,6 +44,9 @@ export class App {
     this.useMiddleware();
     this.useRoutes();
     this.useExeptionFilters();
+
+    await this.prismaService.connect();
+
     this.server = this.app.listen(this.port);
 
     this.logger.log(`Server is running on http://localhost:${this.port}`);
